37b4d9327178ab54dd086fa9781b680df25234e6
Muhammed Ahad <maaahad@gmail.com>
2026-07-30 23:57:19 +0200

    crossterm dependency added and first styled text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+use std::io::{Result, stdout};
+
+fn main() -> Result<()> {
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Red),
+        SetBackgroundColor(Color::Grey),
+        Print("First style with crossterm"),
+        ResetColor
+    )?;
+
+    Ok(())
 }
